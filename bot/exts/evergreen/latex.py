e6485c3fb9c5ee0ae3c0bd5da52f29a2b3f960fe
Hassan Abouelela <hassan@hassanamr.com>
2021-04-14 21:56:19 +0300

    Adds Resource Limits To The Latex Command
    Adds memory and CPU limits to the latex command using processes and the
    resource library, to prevent the command from causing OOM in prod.
    
    Signed-off-by: Hassan Abouelela <hassan@hassanamr.com>

--- bot/exts/evergreen/latex.py
+++ bot/exts/evergreen/latex.py
@@ -1,13 +1,38 @@
 import asyncio
 import hashlib
+import logging
 import pathlib
 import re
-from concurrent.futures import ThreadPoolExecutor
+import signal
+import types
+import typing
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 from io import BytesIO
 
 import discord
 import matplotlib.pyplot as plt
 from discord.ext import commands
+
+from bot import constants
+
+# Import resource management utilities, but only on Unix
+LOAD_COG = True
+
+try:
+    import resource
+    USE_RESOURCE = True
+except ModuleNotFoundError:
+    if not constants.Client.debug:
+        logging.getLogger(__name__).warning(
+            "Could not get `resource` for the latex command. The cog will be disabled. "
+            "If this is intentional, enable the `DEBUG` flag."
+        )
+
+        # Set this flag to prevent the cog from being added when resource is required.
+        LOAD_COG = False
+
+    USE_RESOURCE = False
 
 # configure fonts and colors for matplotlib
 plt.rcParams.update(
@@ -69,6 +94,52 @@ class Latex(commands.Cog):
         else:
             return text
 
+    @staticmethod
+    def _bound_render(
+        render: typing.Callable[[], BytesIO],
+        cpu_limit: int = 5,
+        mem_limit: int = 10
+    ) -> typing.Union[BytesIO, str]:
+        """
+        Calls Latex._render with safe usage limits.
+
+        `cpu_limit` is the CPU execution time limit in seconds, default 5s.
+        `mem_limit` is the memory limit in mb, default 10mb.
+
+        Returns the result of _render, or a string if the process failed.
+        """
+        # Convert to bytes
+        mem_limit *= 1E6
+
+        class CPUError(Exception):
+            pass
+
+        # CPU limit error handler
+        def cpu_handler(_signum: signal.Signals, _frame: typing.Optional[types.FrameType]) -> None:
+            raise CPUError()
+        signal.signal(signal.SIGXCPU, cpu_handler)
+
+        # Ensure the hard limit will not exceed the current one.
+        current_cpu = resource.getrlimit(resource.RLIMIT_CPU)
+        current_mem = resource.getrlimit(resource.RLIMIT_AS)
+
+        if current_cpu[1] != -1 and cpu_limit > current_cpu[1]:
+            cpu_limit = current_cpu[1]
+        if current_mem[1] != -1 and mem_limit > current_mem[1]:
+            mem_limit = current_mem[1]
+
+        # Update the limits
+        resource.setrlimit(resource.RLIMIT_CPU, (cpu_limit, current_cpu[1]))
+        resource.setrlimit(resource.RLIMIT_AS, (mem_limit, current_mem[1]))
+
+        # TODO: Figure out why the except blocks aren't called  # noqa: Why can't I have todos
+        try:
+            return render()
+        except MemoryError:
+            return "Your input exceeded the allowed memory limit. Aborting."
+        except CPUError:
+            return "Your input exceeded the allowed CPU limit. Aborting."
+
     @commands.command()
     @commands.max_concurrency(1, commands.BucketType.guild, wait=True)
     async def latex(self, ctx: commands.Context, *, text: str) -> None:
@@ -81,14 +152,28 @@ class Latex(commands.Cog):
                 await ctx.send(file=discord.File(image_path))
                 return
 
-            with ThreadPoolExecutor() as pool:
-                image = await asyncio.get_running_loop().run_in_executor(
-                    pool, self._render, text, image_path
-                )
+            with ProcessPoolExecutor(1) as pool:
+                loop = asyncio.get_running_loop()
+                function = partial(self._render, text, image_path)
 
-            await ctx.send(file=discord.File(image, "latex.png"))
+                if USE_RESOURCE:
+                    # Handle cases where the resource library is available
+                    result = await loop.run_in_executor(pool, Latex._bound_render, function)
+
+                    # The helper didn't return, and was caught by an error handler.
+                    if isinstance(result, str):
+                        await ctx.send(result)
+                        return
+
+                else:
+                    # Handle cases where the resource library is not available
+                    result = await loop.run_in_executor(pool, function)
+
+            await ctx.send(file=discord.File(result, "latex.png"))
 
 
 def setup(bot: commands.Bot) -> None:
     """Load the Latex Cog."""
-    bot.add_cog(Latex(bot))
+    # Disable the cog if the resource library is not available, and the debug flag is not set.
+    if LOAD_COG:
+        bot.add_cog(Latex(bot))
